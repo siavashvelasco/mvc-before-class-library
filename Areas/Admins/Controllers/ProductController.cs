8ca2cde0ebc0870755343ae9b088f6cd75096d73
soheil <sedalisafavi@gmail.com>
2025-02-05 17:21:11 +0330

    before handle image in update

--- Areas/Admins/Controllers/ProductController.cs
+++ Areas/Admins/Controllers/ProductController.cs
@@ -9,10 +9,11 @@ namespace MVC23._10._1403.Areas.Admins.Controllers
 	public class ProductController : Controller
 	{
 		private IUnitOfWork _unitOfWork;
-
-		public ProductController(IUnitOfWork unitOfWork)
+		private IWebHostEnvironment _webHostEnvironment;
+		public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
 		{
 			_unitOfWork = unitOfWork;
+			_webHostEnvironment = webHostEnvironment;
 		}
 
 		public IActionResult Index()
@@ -21,21 +22,38 @@ namespace MVC23._10._1403.Areas.Admins.Controllers
 
 			return View(Products);
 		}
-		public IActionResult Create()
+		public IActionResult Upsert(int? id)
 		{
 			IEnumerable<SelectListItem> categoryListItem = _unitOfWork.CategoryRepo.GetAll()
 				.Select(u => new SelectListItem() { Text = u.Name, Value = u.Id.ToString() });
-			//ViewBag.CategoryListItem = categoryListItem;
-			//ViewData["Title"] = categoryListItem;
-			var categoryListItemToView = new ProductVM() { CategoryListItem = categoryListItem, Product = new Product() };
-			return View(categoryListItemToView);
-		}
-		[HttpPost]
-		public IActionResult Create(ProductVM obj)
-		{
-			if (ModelState.IsValid)
+			var vmProduct = new ProductVM() { CategoryListItem = categoryListItem, Product = new Product() };
+
+			if (id == null || id == 0)
 			{
 
+				return View(vmProduct);
+			}
+			else
+			{
+				var productToEdit = _unitOfWork.ProductRepo.Get(o => o.Id == id);
+				vmProduct.Product = productToEdit;
+				return View(vmProduct);
+
+			}
+		}
+		[HttpPost]
+		public IActionResult Upsert(ProductVM obj,IFormFile? file)
+		{
+			var wwwRoot = _webHostEnvironment.WebRootPath;//آدرس سرور
+			var filePath = Path.Combine(wwwRoot, @"Image\Product");
+			var fileName = /*Guid.NewGuid().ToString()*/ file.FileName;
+			using (var theMainFile = new FileStream(Path.Combine(filePath, fileName), FileMode.Create))
+			{
+				file.CopyTo(theMainFile);
+			}
+			if (ModelState.IsValid)
+			{
+				obj.Product.ImageUrl = "/Image/Product/" + fileName;//آدرس url
 				_unitOfWork.ProductRepo.Create(obj.Product);
 				_unitOfWork.Save();
 				TempData["cerMas"] = "Product creadted sucsessfully";
@@ -48,12 +66,7 @@ namespace MVC23._10._1403.Areas.Admins.Controllers
 
 
 		}
-		public IActionResult Edit(int id)
-		{
-			var Products = _unitOfWork.ProductRepo.Get(o => o.Id == id);
-			return View(Products);
 
-		}
 		[HttpPost]
 		public IActionResult Edit(Product obj)
 		{
--- Program.cs
+++ Program.cs
@@ -21,19 +21,6 @@ if (!app.Environment.IsDevelopment())
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
